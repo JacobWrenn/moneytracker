219b6f1cdf156e04c6b211bf1d82957d977b93de
Jacob Wrenn <jacob@wrenn.me>
2019-07-12 13:01:45 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 const fb = require('./firebaseConfig.js')
 import './assets/scss/app.scss'
+import 'firebaseui/dist/firebaseui.css'
 
 Vue.config.productionTip = false
 
